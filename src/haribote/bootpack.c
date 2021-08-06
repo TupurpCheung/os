5c5692485b153b049169ceebeed373af838e1e97
tupurp <tupurpcheung@gmail.com>
2021-08-06 23:34:52 +0800

    第四天-2：使用c语言指针替换write_mem8操作

--- src/haribote/bootpack.c
+++ src/haribote/bootpack.c
@@ -3,9 +3,12 @@ void io_hlt(void);
 void write_mem8(int addr,int data);
 
 void HariMain(void) {
-	int i;
+	int i;	/**32位的整数，即4个字节DWORD*/
+	char *p;	/**char类型的指针，用于Byte型地址*/
+	
 	for	(i = 0xa0000;	i <= 0xaffff;	i++)	{
-		write_mem8(i,i | 0x0f);	/*MOV BYTE[i],15*/
+		p = i;
+		*p = i | 0x0f;
 	}
 	
 	for (;;) {
